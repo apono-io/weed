1223c4358fd83fa2302f84511cf7e064ba03bcab
Dima Geyets <dima.g@apono.io>
2022-06-09 20:18:11 +0300

    cli cleanup

--- cmd/weed/main.go
+++ cmd/weed/main.go
@@ -2,72 +2,9 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/apono-io/weed/pkg/core"
 )
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/iam"
-	"github.com/davecgh/go-spew/spew"
-)
-
-func PrintError(err error) {
-	fmt.Fprintf(os.Stderr, "error: %v\n", err)
-	os.Exit(1)
-}
-
-type ArrayOfStrings []string
-
-// UnmarshalJSON implements the json.Unmarshaler interface
-func (aos *ArrayOfStrings) UnmarshalJSON(b []byte) error {
-
-	var actions []string
-
-	if b[0] == '"' {
-		var action string
-		if err := json.Unmarshal(b, &action); err != nil {
-			return err
-		}
-		actions = append(actions, action)
-	} else if err := json.Unmarshal(b, &actions); err != nil {
-		return err
-	}
-
-	*aos = actions
-
-	return nil
-}
-
-type PolicyPermissionStatement struct {
-	Effect   string         `json:"Effect"`
-	Action   ArrayOfStrings `json:"Action"`
-	Resource ArrayOfStrings `json:"Resource"`
-	// Condition interface{}    `json:"Condition"` // TODO: add logic to handle this
-}
-
-type PolicyPermission struct {
-	Version   string                      `json:"Version"`
-	Statement []PolicyPermissionStatement `json:"Statement"`
-}
-
-type Diff struct {
-	Added   []string
-	Removed []string
-}
-
-func diff(a, b []string) []string {
-	mb := make(map[string]struct{}, len(b))
-	for _, x := range b {
-		mb[x] = struct{}{}
-	}
-	var diff []string
-	for _, x := range a {
-		if _, found := mb[x]; !found {
-			diff = append(diff, x)
-		}
-	}
-	return diff
-}
 
 func main() {
 	fmt.Println("CLI", core.Version, core.Commit, core.BuildDate)
--- go.mod
+++ go.mod
@@ -3,16 +3,16 @@ module github.com/apono-io/weed
 go 1.18
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.16.5
+	github.com/aws/aws-sdk-go-v2/config v1.15.10
+	github.com/aws/aws-sdk-go-v2/service/iam v1.18.7
+	github.com/google/go-cmp v0.5.8
 	k8s.io/api v0.24.1
 	k8s.io/apimachinery v0.24.1
 	k8s.io/klog/v2 v2.60.1
-	github.com/aws/aws-sdk-go-v2/service/iam v1.18.7
-	github.com/r3labs/diff/v3 v3.0.0
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.16.5 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.15.10 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.12.5 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.6 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.12 // indirect
@@ -36,10 +36,4 @@ require (
 	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
 	sigs.k8s.io/yaml v1.2.0 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/protobuf v1.3.1 // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
-	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
-	golang.org/x/net v0.0.0-20190603091049-60506f45cf65 // indirect
-	google.golang.org/appengine v1.6.6 // indirect
 )
