8f440e4ea35e15a94af040b62886ac34909e022d
Dima Geyets <dima.g@apono.io>
2022-06-10 01:06:28 +0300

    CLI directory renaming

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,7 +4,7 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/apono-io/weed/cmd/weed/cmd"
+import "github.com/apono-io/weed/cmd/cli/cmd"
 
 func main() {
 	cmd.Execute()
